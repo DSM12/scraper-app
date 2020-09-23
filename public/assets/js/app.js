97ec79ea3ed0528617249d77271ac87cd4fd1747
Danny Montano <dsmontano12@gmail.com>
2020-09-23 15:16:48 -0400

    attempting to render scraped data

---
+++ public/assets/js/app.js
@@ -0,0 +1,80 @@
+$(document).on("click", "#scrape", function() {
+
+    $.ajax({
+      method: "GET",
+      url: "/scrape"
+    })
+    console.log(data[i]);
+    res.redirect("/");
+  
+  });
+  
+      $.getJSON("/articles", function(data) {
+  
+      for (var i = 0; i < data.length; i++) {
+          
+          var dataId="<p data-id='" + data[i]._id + "'>";
+          var articleTitle = "<h4>" + data[i].title + "</h4>";
+          var articleSummary = "<p>" + data[i].summary + "</p>";
+          var commentBtn = "<button class='btn btn-outline-success' data-id='" + data[i]._id + "' data-toggle='modal' data-target='#noteModal' id='addComment' type='submit'>Add Comment</button>";
+          var articleLink = "<a href='" + data[i].link + "'>" + "<p>" + data[i].link; + "</a></p>";
+          var note = "";
+          if ('note' in data[i]) {
+            note = data[i].note.title + "<br>" + data[i].note.body;
+          }
+          var noteDiv = "<div id='notes'><p>Comments:<br>" + note + "</p></div>";
+  
+          var articleInfo = dataId + articleTitle + articleSummary + commentBtn + noteDiv + articleLink + "<hr>";
+          $("#articles").append(articleInfo);
+  
+      }
+      // console.log(JSON.stringify(data[i]));
+    });
+  
+    $('#noteModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget);
+    })
+  
+    $(document).on("click", "#addComment", function() {
+       
+      $("#savenote").attr("data-id", $(this).attr("data-id"))
+      var thisId = $(this).attr("data-id");
+      console.log(thisId)
+      $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+      })
+  
+        .then(function(data) {
+          console.log(data);
+  
+          if (data.note) {
+            $("#notes").append("<p>" + data.note.title + "<br>" + data.note.body + "</p>");
+          }
+        });
+    });
+    
+    $(document).on("click", "#savenote", function() {
+  
+      var thisId = $(this).attr("data-id");
+    
+      $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+          title: $("#titleinput").val(),
+          body: $("#bodyinput").val()
+        }
+      })
+  
+        .then(function(data) {
+        
+          window.location.reload(true);
+          // console.log(data);
+          // $("#notes").append(data.note.title + data.note.body);
+        });
+    
+      // Also, remove the values entered in the input and textarea for note entry
+      // $("#titleinput").val("");
+      // $("#bodyinput").val("");
+    });
---
+++ server.js
@@ -0,0 +1,127 @@
+var express = require("express");
+var exphbs = require('express-handlebars');
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var ObjectId = require("mongoose").Types.ObjectId;
+var request = require("request");
+var cheerio = require("cheerio");
+
+var port = process.env.PORT || 3000;
+
+var db = require("./models");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, function(err){
+  if (err) {
+    console.log(err);
+  }
+  else {
+    console.log("connected to mongoose");
+  }
+
+});
+
+
+/////////// Routes
+
+app.get("/", function(req, res) {
+  db.Article.find({})
+  .then(function(dbArticles){
+    res.render("index", { dbArticles: dbArticles });
+    console.log("dbArticles" + dbArticles);
+  }).catch(function(err) {
+    console.log(err);
+  })
+})
+
+// $(document).on("click", "#scrape", function() {
+app.get("/scrape", function(req, res) {
+
+  request("https://www.inquirer.com/economy/", function(error, response, html) {
+    var $ = cheerio.load(html);
+    console.log("html" + html);
+
+    $("article.story").each(function(i, element) {
+      var result = {};
+
+      result.title = $(this)
+          .find("h2.headline").text().trim();
+
+      result.link = $(this)
+          .find("a.story-link").attr("href");
+
+      result.summary = $(this)
+          .find("p.summary").text().trim();
+
+      console.log(result);
+
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          return res.json(err);
+        });
+    })
+  })
+    res.send("Scrape Complete");
+});
+// });
+
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      console.log(dbNote);
+      console.log(req.params.id)
+      return db.Article.findOneAndUpdate({ _id: ObjectId(req.params.id) }, 
+                                        {$set: {note: dbNote._id }}, 
+                                        { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+app.listen(port, function(){
+  console.log('Listening on PORT ' + port);
+});
